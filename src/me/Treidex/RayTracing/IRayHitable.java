94e050ddcb9966a44ca6d7d6956aad04df91c31d
Treidex <telkids4@gmail.com>
2020-08-07 11:36:06 -0700

    Trying to try with Processing

--- src/me/Treidex/RayTracing/IRayHitable.java
+++
@@ -1,9 +0,0 @@
-package me.Treidex.RayTracing;
-
-import org.joml.Vector3f;
-
-public interface IRayHitable
-{
-	public int getColor(Vector3f hitPosition);
-	public boolean isColliding(Vector3f checkPosition);
-}
